b0590d0f7d3ed94400fe4570a2122e9d14ef7fe1
raphaeldodier <raphael.dodier@outlook.com>
2020-02-25 10:13:52 -0500

    test

--- js/fonctions.js
+++ js/fonctions.js
@@ -5,16 +5,16 @@ function accueilImage(heure){
     var vague = d.getElementById("vague");
     var etoile = d.getElementById("etoileFilante");
     if(heure>5 && heure<=16){
-        accueil.style.backgroundImage = "url('./img/accueilAm.png')"
-        vague.style.backgroundImage = "url('./img/accueilAm.png')"
+        accueil.style.backgroundImage = "url('/img/accueilAm.png')"
+        vague.style.backgroundImage = "url('/img/accueilAm.png')"
         etoile.style.display = "none";
     }else if(heure>16 && heure<=19){
-        accueil.style.backgroundImage = "url('./img/accueilPm.png')"
-        vague.style.backgroundImage = "url('./img/accueilPm.png')"
+        accueil.style.backgroundImage = "url('../img/accueilPm.png')"
+        vague.style.backgroundImage = "url('../img/accueilPm.png')"
         etoile.style.display = "none";
     }else{
-        accueil.style.backgroundImage = "url('./img/accueilSoir.png')"
-        vague.style.backgroundImage = "url('./img/accueilSoir.png')"
+        accueil.style.backgroundImage = "url('../img/accueilSoir.png')"
+        vague.style.backgroundImage = "url('../img/accueilSoir.png')"
         nuage.style.display = "none";
     }
 }
